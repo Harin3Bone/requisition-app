12803300ef772a43d62ee5570918ada20d0fce50
Harin3Bone <harin.thananam@gmail.com>
2023-07-27 03:06:15 +0700

    Backend - Initial backend service

---
+++ backend/src/main/java/com/project/requisition/RequisitionApplication.java
@@ -0,0 +1,13 @@
+package com.project.requisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequisitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequisitionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/requisition/RequisitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.requisition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequisitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
